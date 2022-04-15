966ec5ec45be94580df97b239b763df9ac6accb6
abrarjahin18 <jahin.rupu@gmail.com>
2022-04-15 12:01:59 +0600

    Add project files.

---
+++ DonarBd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DonarBd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
