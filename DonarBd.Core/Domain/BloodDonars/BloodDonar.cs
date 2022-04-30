28f99ffaae6bb37822fccef498b11fc928a9c904
abrarjahin18 <jahin.rupu@gmail.com>
2022-04-30 10:39:01 +0600

    fff

---
+++ DonarBd.Core/Domain/BloodDonars/BloodDonar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonarBd.Core.Domain.BloodDonars
+{
+    public class BloodDonar: BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FacebookURL { get; set; }
+        public string Gender { get; set; }//dropdown in frontend
+        public string BloodGroup { get; set; }//dropdown in frontend
+        public string BloodStatus { get; set; }//dropdown in frontend
+        public DateTime BirthDayDate { get; set; }
+        public DateTime LastDonationDate { get; set; }
+        public string PresentDistrict { get; set; }
+        public string PresentCity { get; set; }
+        public string PresentZone { get; set; }
+        public long TotalBloodDonation { get; set; }
+    }
+}
